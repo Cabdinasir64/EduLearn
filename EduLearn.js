9606bac1b16987437fb952c84106df10313109af
Cabdinasir64 <cabdinasira64@gmail.com>
2025-02-26 10:44:35 +0300

    The First Upload Website

---
+++ EduLearn.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.iconb');
+const mobileNav = document.querySelector('.mobile-nav');
+
+menu.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+});
+
+
